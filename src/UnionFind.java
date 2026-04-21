bef5041cd73194b3d54c8390a689430601d52d17
LEONARDO_DIAS <leonardodias2706@gmail.com>
2026-04-21 23:51:37 +0200

    optimizations

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -1,15 +1,34 @@
 public class UnionFind {
   static int[] equiv;
+  static int[] height;
 
   static void init(int len) {
     equiv = new int[len];
+    height = new int[len];
 
-    for (int i = 0; i < len; ++i)
+    for (int i = 0; i < len; ++i) {
       equiv[i] = i;
+      height[i] = 0;
+    }
   }
 
   static int naiveFind(int x) {
     return equiv[x];
+  }
+
+  static int fastFind(int x) {
+    while (x != equiv[x])
+      x = equiv[x];
+    return x;
+  }
+
+  static int logFind(int x) {
+    while (equiv[x] != x) {
+      equiv[x] = equiv[equiv[x]];
+      x = equiv[x];
+    }
+
+    return x;
   }
 
   static int naiveUnion(int x, int y) {
@@ -22,11 +41,36 @@ public class UnionFind {
     return equiv[y];
   }
 
+  static int fastUnion(int x, int y) {
+    x = find(x);
+    y = find(y);
+    equiv[x] = y;
+
+    return y;
+  }
+
+  static int logUnion(int x, int y) {
+    x = find(x);
+    y = find(y);
+
+    if (height[x] < height[y]) {
+      equiv[x] = y;
+      return y;
+    }
+
+    else {
+      equiv[y] = x;
+      if (height[x] == height[y])
+        height[x]++;
+      return x;
+    }
+  }
+
   static int find(int n) {
-    return naiveFind(n);
+    return fastFind(n);
   }
 
   static int union(int x, int y) {
-    return naiveUnion(x, y);
+    return fastUnion(x, y);
   }
 }
